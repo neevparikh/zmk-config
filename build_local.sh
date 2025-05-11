e03fbb7dca7374ba563f3918182f1e4f983cdc10
Neev Parikh <neev@metr.org>
2025-05-11 15:34:15 -0700

    feat: updated keymap

---
+++ build_local.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+cd zmk/
+uv run west build -s app -d "build/corne_left" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="corne_left"
+uv run west build -s app -d "build/corne_right" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="corne_right"
+uv run west build -s app -d "build/cradio_left" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="cradio_left"
+uv run west build -s app -d "build/cradio_right" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="cradio_right"
+cd ..
+rm -rf firmware
+mkdir firmware
+cp zmk/build/corne_left/zephyr/zmk.uf2 firmware/corne_left.uf2
+cp zmk/build/corne_right/zephyr/zmk.uf2 firmware/corne_right.uf2
+cp zmk/build/cradio_left/zephyr/zmk.uf2 firmware/cradio_left.uf2
+cp zmk/build/cradio_right/zephyr/zmk.uf2 firmware/cradio_right.uf2
