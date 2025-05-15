881c1f5fe13646f014ff70f82fd840a69f6e3460
Neev Parikh <neev@metr.org>
2025-05-15 14:32:43 -0700

    feat: happy for now, good hrm and combos are the way

--- build_local.sh
+++ build_local.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd zmk/
-uv run west build -s app -d "build/corne_left" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="corne_left" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers"
-uv run west build -s app -d "build/corne_right" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="corne_right" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers"
+# uv run west build -s app -d "build/corne_left" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="corne_left" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers"
+# uv run west build -s app -d "build/corne_right" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="corne_right" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers"
 uv run west build -s app -d "build/cradio_left" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="cradio_left" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers"
 uv run west build -s app -d "build/cradio_right" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="cradio_right" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers"
 cd ..
