e3f7f2c0c805e092319ca92aa62aa51946b09319
Neev Parikh <neev@metr.org>
2026-01-16 18:36:58 -0800

    feat: Add support for Raytac Dongle

--- build_local.sh
+++ build_local.sh
@@ -3,13 +3,13 @@ cd zmk/
 # uv run west build -s app -d "build/corne_left" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="corne_left" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers"
 # uv run west build -s app -d "build/corne_right" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="corne_right" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers"
 
-uv run west build -s app -d "build/cradio_left" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="cradio_left" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers;$HOME/repos/zmk-config/modules/zmk-component-raytac-dongle" -DCONFIG_ZMK_SPLIT=y -DCONFIG_ZMK_SPLIT_ROLE_CENTRAL=n
-uv run west build -s app -d "build/cradio_right" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="cradio_right" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers;$HOME/repos/zmk-config/modules/zmk-component-raytac-dongle" -DCONFIG_ZMK_SPLIT=y -DCONFIG_ZMK_SPLIT_ROLE_CENTRAL=n
+uv run west build -s app -d "build/cradio_left" -b "nice_nano"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="cradio_left" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers;$HOME/repos/zmk-config/modules/zmk-component-raytac-dongle" -DCONFIG_ZMK_SPLIT=y -DCONFIG_ZMK_SPLIT_ROLE_CENTRAL=n
+uv run west build -s app -d "build/cradio_right" -b "nice_nano"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="cradio_right" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers;$HOME/repos/zmk-config/modules/zmk-component-raytac-dongle" -DCONFIG_ZMK_SPLIT=y -DCONFIG_ZMK_SPLIT_ROLE_CENTRAL=n
 
-uv run west build -s app -d "build/cradio_dongle" -b "raytac_mdbt50q_rx"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="cradio_dongle" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers;$HOME/repos/zmk-config/modules/zmk-component-raytac-dongle"
+uv run west build -s app -d "build/cradio_dongle" -b "mdbt50q_rx"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="cradio_dongle" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers;$HOME/repos/zmk-config/modules/zmk-component-raytac-dongle"
 
-uv run west build -s app -d "build/settings_reset_dongle" -b "raytac_mdbt50q_rx"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="settings_reset" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers;$HOME/repos/zmk-config/modules/zmk-component-raytac-dongle"
-uv run west build -s app -d "build/settings_reset" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="settings_reset" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers;$HOME/repos/zmk-config/modules/zmk-component-raytac-dongle"
+uv run west build -s app -d "build/settings_reset_dongle" -b "mdbt50q_rx"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="settings_reset" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers;$HOME/repos/zmk-config/modules/zmk-component-raytac-dongle"
+uv run west build -s app -d "build/settings_reset" -b "nice_nano"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="settings_reset" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers;$HOME/repos/zmk-config/modules/zmk-component-raytac-dongle"
 
 cd ..
 rm -rf firmware
