3abd92bb756dda80415b44bd8b81c81448f532ba
Neev Parikh <neev@metr.org>
2026-01-16 18:12:29 -0800

    feat: added new data

--- build_local.sh
+++ build_local.sh
@@ -4,6 +4,7 @@ cd zmk/
 # uv run west build -s app -d "build/corne_right" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="corne_right" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers"
 uv run west build -s app -d "build/cradio_left" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="cradio_left" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers"
 uv run west build -s app -d "build/cradio_right" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="cradio_right" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers"
+uv run west build -s app -d "build/settings_reset" -b "nice_nano_v2"  -- -DZMK_CONFIG="$HOME/repos/zmk-config/config" -DSHIELD="settings_reset" -DZMK_EXTRA_MODULES="$HOME/repos/zmk-config/modules/zmk-helpers"
 cd ..
 rm -rf firmware
 mkdir firmware
@@ -11,3 +12,4 @@ mkdir firmware
 # cp zmk/build/corne_right/zephyr/zmk.uf2 firmware/corne_right.uf2
 cp zmk/build/cradio_left/zephyr/zmk.uf2 firmware/cradio_left.uf2
 cp zmk/build/cradio_right/zephyr/zmk.uf2 firmware/cradio_right.uf2
+cp zmk/build/settings_reset/zephyr/zmk.uf2 firmware/settings_reset.uf2
